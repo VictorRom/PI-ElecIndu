717e61af720e0f3eaf67d9a9e6b2931382e49fe4
alphasigma25 <61196412+alphasigma25@users.noreply.github.com>
2023-03-16 16:46:51 +0100

    Cbor to python

---
+++ transformer/CborToJsonTest.py
@@ -0,0 +1,18 @@
+import cbor2 as cb2
+import CborToJson
+
+# transform a cbor file to geojson
+
+dict = {"coordinates": [
+          8.134863519050327,
+          47.02492394298815
+        ],
+        "serial" : 123,
+        "other" : "test"}
+
+rawdata = cb2.dumps(dict)
+
+CborToJson.getJSON(rawdata)
+
+assert(CborToJson.getJSON(rawdata) == {'type': 'Feature', 'properties': {}, 'geometry': {'coordinates': [8.134863519050327, 47.02492394298815], 'type': 'Point'}})
+
