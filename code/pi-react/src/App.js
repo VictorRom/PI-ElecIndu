4c8d138cdac99da9d874c2e8b3cfa687116d8714
alphasigma25 <61196412+alphasigma25@users.noreply.github.com>
2023-03-29 22:16:43 +0200

    test

--- code/pi-react/src/App.js
+++ code/pi-react/src/App.js
@@ -1,29 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import MeditrakkerMap from './components/map';
-import FromScratchMap from './components/mapFromScratch';
+import Map3 from './components/map3'
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      {/* <h1>Testos</h1> */}
-      {/*<MeditrakkerMap center_lat={45.39701} center_lon={6.58968} zoom={13} />*/}
-      <FromScratchMap center_lat={45.39701} center_lon={6.58968} zoom={13} />
-      
+      <Map3 />
     </div>
   );
 }
