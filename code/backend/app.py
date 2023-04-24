39915e1f253edf968362ea7db5579171ccdde792
alduinsorn <alduinsorn@gmail.com>
2023-04-24 23:28:53 +0200

    avancement sur les routes mais pas testées, et mise en place en local de ma db mongo

--- code/backend/app.py
+++ code/backend/app.py
@@ -2,14 +2,24 @@ from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
 from pydantic.fields import *
-from datetime import datetime
+from datetime import datetime, timedelta
 import pymongo as pm
 
+
+mongodb_connection_string = 'mongodb://localhost:27017'
+client = pm.MongoClient(mongodb_connection_string)
+db = client['elecindu']
+collection = db['gps']
+
+# class GPSData(BaseModel):
+#     lat: float
+#     lon: float
+#     speed: float
+
 class GPSData(BaseModel):
-    lat: float = Field(..., gt=-90, lt=90)
-    lon: float = Field(..., gt=-180, lt=180)
-    speed: float = Field(..., gt=0)
-    dt: datetime = Field(...) # quand on ajoute des données dans la db, on ajoute le timestamp
+    type: str
+    geometry: dict
+    properties: dict
 
 app = FastAPI()
 
@@ -23,13 +33,22 @@ async def root():
 @app.get("/live")
 async def live_page():
     # TODO get the last 6 * 24 points from the database and return them (1 point every 10 minutes for the last 24 hours or for the last day)
-    return
+    # return {"a": "1"}
+    query = {"properties.timestamp": {"$gte": datetime.now() - timedelta(days=1)}}
+    results = collection.find(query)
 
-@app.get("/trail/dts={dts}&dte={dte}") # dts = date time start, dte = date time end
+    return {"points": list(results)}
+
+@app.get("/trail/dts={dts}&dte={dte}") # on passe les data en json sans passer par le body ?
 async def trail_page(dts: datetime, dte: datetime):
     # TODO get the points between dts and dte from the database and return them
+    # verify that the dates are valid (dts < dte)
+    query = {"properties.timestamp": {"$gte": dts, "$lte": dte}}
+    results = collection.find(query)
+
     # TODO compute some statistics on the data (distance, avg speed, total time) and return them 
-    return
+
+    return {"points": list(results), "statistics": {"distance": 0, "avg_speed": 0, "total_time": 0}}
 
 @app.get("/sessions") # A VOIR SI ON FAIT TJR ÇA ?
 async def sessions_page():
@@ -41,10 +60,13 @@ async def sessions_page():
 # POST ROUTES #
 ###############
 @app.post("/gps", response_description="GPS data received")
-async def receive_gps_data(data: GPSData):
+async def receive_gps_data(data: bytes):
     # TODO process the data, convert to GeoJSON, and store in MongoDB
+    tls_key = "key.pem"
+    # decode the data using the tls key
+    # transfer the data to the transformer
     return {"message": "Data received"}
 
-
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, ssl_keyfile="key.pem", ssl_certfile="cert.pem")
+    uvicorn.run("app:app", host="0.0.0.0", port=8080)#, ssl_keyfile="key.pem", ssl_certfile="cert.pem")
+
