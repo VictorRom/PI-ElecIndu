45e87f25cb2325c330ee8af7b4153c5e8af9e532
alphasigma25 <61196412+alphasigma25@users.noreply.github.com>
2023-05-25 15:22:46 +0200

    route tester adaptation

--- code/backend/route_tester.py
+++ code/backend/route_tester.py
@@ -2,24 +2,31 @@
 import requests # pip install requests
 from datetime import datetime
 
-baseURL = "http://localhost:8080"
+baseURL = "http://0.0.0.0:5050"
 
 def test_trail(dts, dte):
-    r = requests.get("{base}/trail/dts={dts}&dte={dte}".format(base=baseURL,dts=dts, dte=dte))
-    print(r.json())
+    r = requests.get("{base}/trail/dts={dts}&dte={dte}&proto=1".format(base=baseURL,dts=dts, dte=dte))
+    if r.status_code == 200:
+        print(r.json())
+    else :
+        print(r.status_code)
 
 def test_live():
-    r = requests.get("{base}/live".format(base=baseURL))
-    print(r.json())
+    r = requests.get("{base}/live/proto=1".format(base=baseURL))
+    if r.status_code == 200:
+        print(r.json())
+    else :
+        print(r.status_code)
 
 def init_db():
-    requests.get("{base}/insert_data".format(base=baseURL))
+    r = requests.get("{base}/insert_data".format(base=baseURL))
+    print(r.status_code)
 
 
 if __name__ == "__main__":
-    start = datetime(2023, 4, 25, 16, 0, 0)
+    start = datetime(2023, 4, 25, 0, 0, 0)
     print(start)
-    end = datetime(2023, 4, 25, 16, 30, 0)
+    end = datetime(2023, 4, 25, 0, 0, 0)
     init_db()
     test_live()
     test_trail(start, end)
