9f0aaa63c00e1d931ba9298d55a44f063188fbdd
alphasigma25 <61196412+alphasigma25@users.noreply.github.com>
2023-06-18 19:12:38 +0200

    route tester updated

--- code/backend/route_tester.py
+++ code/backend/route_tester.py
@@ -7,19 +7,23 @@ baseURL = "http://localhost:5050"
 def test_trail(dts, dte):
     r = requests.get(f"{baseURL}/trail/dts={dts}&dte={dte}&proto=1")
     if r.status_code == 200:
-        print(r.json())
+        pass #print(r.json()[:10])
     else :
         print(r.status_code)
 
 def test_live():
     r = requests.get(f"{baseURL}/live/proto=1")
     if r.status_code == 200:
-        print(r.json())
+        pass #print(r.json()[:10])
     else :
         print(r.status_code)
 
 def init_db():
     r = requests.get("{base}/insert_data".format(base=baseURL))
+    print(r.status_code)
+
+def test_delete(dts, dte):
+    r = requests.delete(f"{baseURL}/gps/delete/dts={dts}&dte={dte}&proto=1")
     print(r.status_code)
 
 
@@ -31,3 +35,4 @@ if __name__ == "__main__":
     init_db()
     test_live()
     test_trail(start, end)
+    #test_delete(start, end)
